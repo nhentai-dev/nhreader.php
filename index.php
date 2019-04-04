f920d73ceb3fe953c9b883f780c68de0e0805e48
iqbal rifai <iqbalrifai@waifu.club>
2019-04-04 22:16:52 +0700

    fix Generate Zip

---
+++ index.php
@@ -0,0 +1,118 @@
+<?php ${"G\x4c\x4fB\x41L\x53"}["h\x65\x6fe\x77t\x5fo\x67i\x61x\x64q\x65u\x79u\x64p\x66\x74m\x7aq\x68"]="\x69d";${"G\x4c\x4f\x42\x41L\x53"}["\x6a\x6en\x65\x72\x67\x62\x75u\x63\x6bj\x77\x64\x66\x76r\x64\x68\x65\x6f\x77\x73z"]="_\x47E\x54";${"G\x4c\x4fB\x41\x4cS"}["\x77r\x74p\x61g\x69v\x76e\x69w\x70\x5f\x6d\x78\x77\x64j\x75\x73\x74d\x78\x70k\x74r\x63j\x64n\x76\x6b_\x6bc\x6e\x5fn"]="\x75r\x6c\x31";${"G\x4c\x4fB\x41L\x53"}["\x76\x62w\x5fe\x78\x74\x72\x7a\x78m\x64i\x68\x5f\x6bg\x71\x69i\x6bk\x77m\x5ft\x75\x6f"]="\x75r\x6c\x32";${"G\x4c\x4fB\x41L\x53"}["z\x6ee\x77\x66\x72\x79\x77y\x6bh\x6c\x64\x6e\x5f\x6fv\x71o\x63g\x6cz\x75s\x66\x78u\x78m"]="\x6a\x73o\x6e";${"G\x4c\x4f\x42\x41L\x53"}["_\x6b\x78\x6aw\x75o\x5f\x78q\x63f\x61\x74k\x6d\x6fu\x72\x6dx\x5fw\x63\x64r\x74k\x6b\x78e\x78n"]="\x6as\x6fn\x32";${"\x47\x4cO\x42\x41L\x53"}["\x6br\x70\x70\x64\x75r\x71c\x6bc\x72\x6e\x62g\x67\x64\x6d\x66z\x5fo\x6cf\x73z\x6ey\x76a\x63u\x5f\x6bw\x61\x73\x6c\x62"]="\x6as\x6fn\x5f\x64\x61t\x612";${"\x47\x4cO\x42A\x4cS"}["b\x69\x75\x76x\x6b\x62b\x7am\x5f\x66_\x64t\x61x\x78\x67l\x75t"]="j\x73o\x6e\x5fd\x61t\x61";${"G\x4cO\x42\x41\x4c\x53"}["f\x70g\x79\x61l\x7ap\x67s\x74y\x6d\x65z\x6dg\x5fi\x6bn\x71c\x6bc\x73m\x64n\x5fs\x67g\x71v\x6a_\x65\x61"]="j\x73o\x6e\x5f\x63o\x75n\x74";${"G\x4c\x4fB\x41L\x53"}["o\x61k\x65m\x72_\x6e\x61t\x5f\x71\x74\x75w\x72f\x70\x6fr\x71\x75b\x5fz\x61\x68a\x66d\x67\x5fq"]="\x64\x6fw\x6el\x6fa\x64";${"\x47L\x4fB\x41L\x53"}["\x70\x64s\x69m\x6di\x72w\x6ez\x72\x66\x6b\x61e\x65\x76q\x63h\x63v\x79z\x6d\x72\x5f\x70m\x70l\x6b\x61"]="s\x69\x7a\x65";${"\x47L\x4fB\x41L\x53"}["_\x71\x65\x71s\x69\x66x\x78g\x79_\x7an\x61\x72a\x6e\x6bb\x6ad\x69s"]="y\x75m\x6dy";header("Access-Control-Allow-Origin: *");${${"G\x4c\x4fB\x41L\x53"}["h\x65\x6fe\x77t\x5fo\x67i\x61x\x64q\x65u\x79u\x64p\x66\x74m\x7aq\x68"]}=${${"\x47L\x4f\x42A\x4c\x53"}["\x6a\x6en\x65\x72\x67\x62\x75u\x63\x6bj\x77\x64\x66\x76r\x64\x68\x65\x6f\x77\x73z"]}['g'];if(isset(${${"G\x4c\x4f\x42A\x4c\x53"}["\x6a\x6en\x65\x72\x67\x62\x75u\x63\x6bj\x77\x64\x66\x76r\x64\x68\x65\x6f\x77\x73z"]}['g'])){${${"G\x4c\x4f\x42\x41L\x53"}["\x77r\x74p\x61g\x69v\x76e\x69w\x70\x5f\x6d\x78\x77\x64j\x75\x73\x74d\x78\x70k\x74r\x63j\x64n\x76\x6b_\x6bc\x6e\x5fn"]}="\x68\x74\x74\x70\x73\x3a\x2f\x2f\x61\x70\x69\x73\x2e\x6e\x68\x65\x6e\x74\x2e\x61\x69\x2f\x67\x2f${${"\x47L\x4f\x42A\x4cS"}["h\x65\x6fe\x77t\x5fo\x67i\x61x\x64q\x65u\x79u\x64p\x66\x74m\x7aq\x68"]}";${${"G\x4cO\x42A\x4c\x53"}["\x76\x62w\x5fe\x78\x74\x72\x7a\x78m\x64i\x68\x5f\x6bg\x71\x69i\x6bk\x77m\x5ft\x75\x6f"]}="\x68\x74\x74\x70\x73\x3a\x2f\x2f\x6e\x68\x65\x6e\x74\x61\x69\x2e\x61\x70\x69\x2e\x6c\x65\x6d\x6f\x6e\x7a\x64\x65\x76\x2e\x78\x79\x7a\x2f\x61\x70\x69\x2f\x67\x61\x6c\x6c\x65\x72\x79\x2f${${"G\x4cO\x42A\x4cS"}["h\x65\x6fe\x77t\x5fo\x67i\x61x\x64q\x65u\x79u\x64p\x66\x74m\x7aq\x68"]}";}else{echo "";}${${"G\x4cO\x42A\x4c\x53"}["z\x6ee\x77\x66\x72\x79\x77y\x6bh\x6c\x64\x6e\x5f\x6fv\x71o\x63g\x6cz\x75s\x66\x78u\x78m"]}=file_get_contents(${${"\x47L\x4fB\x41\x4cS"}["\x77r\x74p\x61g\x69v\x76e\x69w\x70\x5f\x6d\x78\x77\x64j\x75\x73\x74d\x78\x70k\x74r\x63j\x64n\x76\x6b_\x6bc\x6e\x5fn"]});${${"G\x4cO\x42A\x4cS"}["_\x6b\x78\x6aw\x75o\x5f\x78q\x63f\x61\x74k\x6d\x6fu\x72\x6dx\x5fw\x63\x64r\x74k\x6b\x78e\x78n"]}=file_get_contents(${${"G\x4c\x4fB\x41\x4c\x53"}["\x76\x62w\x5fe\x78\x74\x72\x7a\x78m\x64i\x68\x5f\x6bg\x71\x69i\x6bk\x77m\x5ft\x75\x6f"]});${${"G\x4cO\x42A\x4cS"}["\x6br\x70\x70\x64\x75r\x71c\x6bc\x72\x6e\x62g\x67\x64\x6d\x66z\x5fo\x6cf\x73z\x6ey\x76a\x63u\x5f\x6bw\x61\x73\x6c\x62"]}=json_decode(${${"G\x4cO\x42A\x4cS"}["_\x6b\x78\x6aw\x75o\x5f\x78q\x63f\x61\x74k\x6d\x6fu\x72\x6dx\x5fw\x63\x64r\x74k\x6b\x78e\x78n"]},true);${${"\x47L\x4f\x42A\x4c\x53"}["b\x69\x75\x76x\x6b\x62b\x7am\x5f\x66_\x64t\x61x\x78\x67l\x75t"]}=json_decode(${${"G\x4c\x4f\x42\x41L\x53"}["z\x6ee\x77\x66\x72\x79\x77y\x6bh\x6c\x64\x6e\x5f\x6fv\x71o\x63g\x6cz\x75s\x66\x78u\x78m"]},true);${${"G\x4cO\x42A\x4c\x53"}["f\x70g\x79\x61l\x7ap\x67s\x74y\x6d\x65z\x6dg\x5fi\x6bn\x71c\x6bc\x73m\x64n\x5fs\x67g\x71v\x6a_\x65\x61"]}=count(${${"G\x4c\x4f\x42\x41L\x53"}["b\x69\x75\x76x\x6b\x62b\x7am\x5f\x66_\x64t\x61x\x78\x67l\x75t"]});${${"\x47L\x4f\x42A\x4c\x53"}["o\x61k\x65m\x72_\x6e\x61t\x5f\x71\x74\x75w\x72f\x70\x6fr\x71\x75b\x5fz\x61\x68a\x66d\x67\x5fq"]}=${${"G\x4cO\x42\x41\x4c\x53"}["b\x69\x75\x76x\x6b\x62b\x7am\x5f\x66_\x64t\x61x\x78\x67l\x75t"]}["dlurl"];${${"\x47L\x4f\x42\x41\x4cS"}["\x70\x64s\x69m\x6di\x72w\x6ez\x72\x66\x6b\x61e\x65\x76q\x63h\x63v\x79z\x6d\x72\x5f\x70m\x70l\x6b\x61"]}=${${"\x47\x4c\x4fB\x41L\x53"}["b\x69\x75\x76x\x6b\x62b\x7am\x5f\x66_\x64t\x61x\x78\x67l\x75t"]}["size"];${${"\x47\x4c\x4fB\x41\x4cS"}["_\x71\x65\x71s\x69\x66x\x78g\x79_\x7an\x61\x72a\x6e\x6bb\x6ad\x69s"]}=${${"G\x4cO\x42A\x4c\x53"}["b\x69\x75\x76x\x6b\x62b\x7am\x5f\x66_\x64t\x61x\x78\x67l\x75t"]}; ?>
+<!DOCTYPE html>
+<html lang="en" class=" theme-black">
+  <head>
+    <link rel="icon" type="image/x-icon" class="js-site-favicon" href="https://cdn.glitch.com/9cf1a9cd-4223-4d68-8e9d-188892573aae%2Fgo-nh.png?1547725929082">
+      <title>Read and Download <?php echo $json_data2["title"]["pretty"];?> &raquo; nhentai: hentai doujinshi and manga</title>
+      <meta charset="UTF-8">
+      <meta itemprop="image" content="<?php echo $json_data["pages"][0];?>"/>
+      <meta itemprop="name" content="<?php echo $json_data2["title"]["pretty"];?>" />
+      <meta property="og:type" content="article" />
+	<meta property="og:title" content="<?php echo $json_data2["title"]["pretty"];?>" />
+	<meta property="og:image" content="<?php echo $json_data["pages"][0];?>" />
+	<meta name="description" content="Read and Download <?php echo $json_data2["title"]["pretty"];?>, a hentai doujinshi by <?php print $json_data["details"]["artists"][0];?> for free on nhentai." />
+	<meta name="twitter:card" content="summary" />
+	<meta name="twitter:title" content="<?php echo $json_data2["title"]["pretty"];?>" />
+	<meta name="twitter:description" content="<?php $a = 0 ; $b = $json_data["details"]["tags"];while($b[$a]){echo $b[$a];$a ++;};?>" />
+      <meta name="ROBOTS" content="NOINDEX, NOFOLLOW"/>
+      <script type="text/javascript" src="https://reader.nhent.ai/js/ga.js"></script>
+      <meta name="theme-color" content="#1f1f1f">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Noto+Sans:400,400i,700">
+      <link rel="stylesheet" href="https://static.nhent.ai/css/main_style.css">
+      <script type="text/javascript" src="//code.jquery.com/jquery-latest.min.js"></script>
+      <script type="text/javascript" src="//stuk.github.io/jszip/dist/jszip.min.js"></script>
+      <script type="text/javascript" src="//stuk.github.io/jszip-utils/dist/jszip-utils.js"></script>
+      <script type="text/javascript" src="//stuk.github.io/jszip/vendor/FileSaver.js"></script>
+      <style>
+         #thumbnail-container img {
+				width: 100%;
+				margin: 10px 0;
+			}
+			.text-jalan {
+			    background: #000;
+			    position: fixed;
+			    bottom: 0;
+			    width: 100%;
+			    z-index: 999;
+			}
+      </style>
+   </head>
+   <body>
+       <div class="text-jalan"><marquee behavior="scroll" direction="left">Peringatan!!! Download, Tags, Search, Login/Register, Home, dll belum tersedia atau sedang dalam perkembangan</marquee></div>
+       <nav role="navigation"><a class="logo" href="/"><img src="https://cdn.glitch.com/9cf1a9cd-4223-4d68-8e9d-188892573aae%2Fgo-nh.png?1547725929082" alt="logo" width="46" height="30"></a><form role="search" action="/search/" class="search"><input type="search" name="q" value="" autocapitalize="none" required /><button type="submit" class="btn btn-primary btn-square"><i class="fa fa-search fa-lg"></i></button></form><button type="button" class="btn btn-secondary btn-square" id="hamburger"><span class="line"></span><span class="line"></span><span class="line"></span></button><div class="collapse"><ul class="menu left"><li class="desktop "><a href="/random/">Random</a></li><li class="desktop "><a href="/tags/">Tags</a></li><li class="desktop "><a href="/artists/">Artists</a></li><li class="desktop "><a href="/characters/">Characters</a></li><li class="desktop "><a href="/parodies/">Parodies</a></li><li class="desktop "><a href="/groups/">Groups</a></li><li class="desktop "><a href="/info/">Info</a></li><li class="dropdown"><button class="btn btn-secondary btn-square" type="button" id="dropdown"><i class="fa fa-chevron-down"></i></button><ul class="dropdown-menu"><li><a href="/random/">Random</a></li><li><a href="/tags/">Tags</a></li><li><a href="/artists/">Artists</a></li><li><a href="/characters/">Characters</a></li><li><a href="/parodies/">Parodies</a></li><li><a href="/groups/">Groups</a></li><li><a href="/info/">Info</a></li></ul></li></ul><ul class="menu right"><li><a href="/login/"><i class="fa fa-sign-in"></i> Sign in</a></li><li><a href="/register/"><i class="fa fa-pencil-square"></i> Register</a></li></ul></div></nav>
+       <div id="content">
+      <div class="container" id="bigcontainer">
+          <div id="cover">
+				<img is="lazyload-image" class="lazyload" width="350" height="497" data-src="<?php echo $json_data["pages"][0];?>" src="<?php echo $json_data["pages"][0];?>" /><noscript><img src="<?php echo $json_data["pages"][0];?>" width="350" height="497" /></noscript>
+		</div>
+         <div id="info-block">
+			<div id="info">
+                     <h1><?php echo $json_data2["title"]["english"];?></h1>
+                     <h1><?php echo $json_data2["title"]["japanese"];?></h1>
+                  <section id="tags">
+					<div class="tag-container field-name ">
+					    Tags:
+						<span class="tags"><?php $a = 0 ; $b = $json_data["details"]["tags"];while($b[$a]){echo $b[$a];$a ++;};?></span>
+					</div>
+					<div class="tag-container field-name ">
+						Artists:
+						<span class="tags"><?php print $json_data["details"]["artists"][0];?></span>
+                  </div>
+                  <div class="tag-container field-name ">
+						Groups:
+                     <span class="tags"><?php print $json_data["details"]["groups"][0];?></span>
+                  </div>
+                  <div class="tag-container field-name ">
+                      Languages:
+                     <span class="tags"><?php $a = 0 ; $b = $json_data["details"]["languages"];while($b[$a]){echo $b[$a];$a ++;};?></span>
+                  </div>
+                  <div class="tag-container field-name ">
+                      Categories:
+                     <span class="tags"><?php print $json_data["details"]["categories"][0];?></span>
+                  </div>
+               </div>
+               <div class="buttons">
+                   <a href="#" id="download" onclick="downloads(urls,nama)" class="btn btn-secondary"><i class="fa fa-download"></i> Download</a>
+               </div>
+              </div> 
+              </div>
+            <div class="container" id="thumbnail-container">
+            </div>
+            <script>
+var prefix='https://i.bakaa.me/galleries/<?php echo $json_data2["media_id"];?>/';var $container=document.getElementById('thumbnail-container');var pageIndex=0;const PAGE_SIZE=2;const TIMEOUT=3*60*1000;loadPage();async function loadPage(){await Promise.race([getSuccessPromise(),getErrorPromise()]);pageIndex+=1;loadPage()}function getSuccessPromise(){var numberList=Array.from(Array(PAGE_SIZE),(a,b)=>b);var promiseList=numberList.map(number=>{var index=PAGE_SIZE*pageIndex+number+1;return new Promise(async resolve=>{await loadImg(index);resolve()})});var successPromise=Promise.all(promiseList);return successPromise}function getErrorPromise(){return new Promise(resolve=>{setTimeout(()=>{resolve()},TIMEOUT)})}async function loadImg(index){return new Promise(resolve=>{var img=createImg(index);img.onload=function(){resolve()};$container.appendChild(img)})}function createImg(index){var img=document.createElement('img');img.onerror=loadPngAfterJpgError.bind(undefined,img,index);var src=prefix+index+'.jpg';img.src=src;return img}function loadPngAfterJpgError(img,index){var src=prefix+index+'.png';img.src=src;img.onerror=function(a,b,c){console.log('png'+index+'----',a,b,c)}}$(document)['ready'](function(){$('#hamburger')['on']('click',function(_0xab55x4){$('.collapse')['toggleClass']('open')})});$(document)['ready'](function(){$('#dropdown')['on']('click',function(_0xab55x4){$('.dropdown-menu')['toggleClass']('open')})})
+            </script>
+            <!-- API HERE -->
+            <script>
+var gallery = new nhtai.gallery(<?php print json_encode($json_data2);?>);gallery.init();
+            </script>
+            <!-- END of API -->
+            <script>
+var urls=<?php print json_encode($json_data["pages"]);?>;var nama="<?php echo $json_data2['title']['japanese'];?> ♥ nHentai";function downloads(urls,nama){var zip=new JSZip();var count=0;var name=nama+".zip";urls.forEach(function(url){JSZipUtils.getBinaryContent(url,function(err,data){if(err){throw err}var filename=url.replace(/.*\//g,'');var img=zip.folder("<?php echo $json_data2['title']['pretty'];?>");img.file("info.txt","Download from https://reader.nhent.ai/g/<?php echo $json_data2['id'];?> \n Dont Forget to join discord: https://discord.gg/y6kurbu").file(filename,data,{binary:true});count+=1;if(count==urls.length){zip.generateAsync({type:'blob'}).then(function(content){saveAs(content,name)})}})})}
+            </script>
+            <script>
+                function setCookie(cname, cvalue, ex) {
+    var d = new Date();
+    d.setTime(d.getTime() + (ex*60*1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + "; " + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+            </script>
+   </body>
+</html>
